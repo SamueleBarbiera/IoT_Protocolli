23b4bd860ccd55d14876b8d42464567e21ca9ad3
samuelebarbiera <barbierasamuele01@gmail.com>
2021-12-14 12:41:33 +0100

    third comm

--- server/server.js
+++ server/server.js
@@ -1,18 +1,15 @@
-// Importing required modules
 const cors = require('cors');
 const express = require('express');
-
 const port = process.env.PORT || 9000;
 const app = express();
 
 require('dotenv').config();
-require("./helpers/db/mongodb.js")();
+require("./db/mongodb.js")();
 app.use(cors());
 app.use(express.json());
 app.set('view engine', 'html');
-app.use(express.static(__dirname + '/views/'));
-app.use('/api', require('./routes/api'));
+app.use('/', require('./routes/drones'));
 app.listen(port);
-console.log(`Listening On http://localhost:${port}`);
+console.log(`Listening On http://localhost:${port}/drones`);
 
 module.exports = app;
