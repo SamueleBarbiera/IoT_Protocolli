a1c433b900e3774b6b1c2ab8b539e1d8b52a50ca
root <root@SAMUELE-TEST.localdomain>
2022-01-25 10:22:49 +0100

    amqp test

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MQTT - REST API - COAP - AMQP 🔮
 
-### i branch funzionali sono: rest-api ,mqtt e amqp usando rabbitMQ
+### i branch funzionali sono: rest-api ,mqtt e amqp usando rabbitMQ leggendo i messaggi della coda
 
 ### per quanto riguarda coap, non siamo riusciti a svolgere la comunicazione, abbiamo provato node,c# senza risultato
 
--- server/server_AMQP.js
+++ server/server_AMQP.js
@@ -1,21 +1,48 @@
-const amqplib = require('amqplib');
-
-const amqp_url = process.env.CLOUDAMQP_URL || 'amqp://10.30.134.27:5672';
+const amqplib = require("amqplib");
+//const Broker = require("rascal").BrokerAsPromised;
+//const config = require("./config.json");
+const amqp_url = process.env.CLOUDAMQP_URL || "amqps://edxgyijm:3xQQbYv2H9ioRo4r_tn3XnVE6upaoRNi@roedeer.rmq.cloudamqp.com/edxgyijm";
 
 async function do_consume() {
     const conn = await amqplib.connect(amqp_url, "heartbeat=60");
-    const ch = await conn.createChannel()
-    const q = 'test_queue';
+    const ch = await conn.createChannel();
+    const q = "posizione";
     await conn.createChannel();
     await ch.assertQueue(q, {durable: true});
-    await ch.consume(q, function (msg) {
-        console.log(msg.content.toString());
-        ch.ack(msg);
-        ch.cancel('myconsumer');
-        }, {consumerTag: 'myconsumer'});
-    setTimeout( function()  {
+    await ch.consume(
+        q,
+        function (msg) {
+            console.log(msg.content.toString());
+            ch.ack(msg);
+            ch.cancel("myconsumer");
+        },
+        {consumerTag: "myconsumer"}
+    );
+    setTimeout(function () {
         ch.close();
-        conn.close();},  500 );
+        conn.close();
+    }, 500);
 }
 
+
 do_consume();
+
+/* Usando un broker
+async function rascal_consume() {
+    console.log("Consuming");
+    const broker = await Broker.create(config);
+    broker.on("error", console.error);
+    const subscription = await broker.subscribe("demo_subscription", "b1");
+    subscription.on("message", (message, content, ackOrNack) => {
+        console.log(content);
+        ackOrNack();
+        subscription.cancel();
+    });
+    subscription.on("error", console.error);
+    subscription.on("invalid_content", (err, message, ackOrNack) => {
+        console.log("Failed to parse message");
+    });
+}
+
+rascal_consume().catch(console.error);
+*/
